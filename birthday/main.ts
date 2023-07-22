dcbe102579cfc3af05ab982a53cad0ffa82ed130
Lorenzo <lovitolorenzo@yahoo.it>
2023-07-22 23:06:24 +0200

    fix: fixes statement to be based on employeeWhoReceivedGreetings lenght

--- birthday/main.ts
+++ birthday/main.ts
@@ -2,10 +2,10 @@ import { BirthdayGreetingService } from "./service";
 
 (async function main(): Promise<void> {
 	const birthdayService = new BirthdayGreetingService("lovitolorenzo23@gmail.com", "smtp.gmail.com", 587);
-	const employeeWhoReceivedGreetings = await birthdayService.findEmployeesBirthdaysAndSendEmails("./employeeData.txt");
+	const employeeWhoReceivedGreetings = await birthdayService.findEmployeesBirthdaysAndSendEmails("employeeData.txt");
 
 	console.log(
-		employeeWhoReceivedGreetings
+		employeeWhoReceivedGreetings.length > 0
 			? `Employees who have received the greetings email: ${employeeWhoReceivedGreetings
 					.map((employee) => `${employee.firstName} ${employee.lastName}`)
 					.join(", ")}`
