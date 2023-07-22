27d6fdc2315ad01b59e05991b9c63abc8c12841c
Lorenzo <lovitolorenzo@yahoo.it>
2023-07-22 16:09:25 +0200

    chore: prints employees that have received some greetings if there are any

--- birthday/main.ts
+++ birthday/main.ts
@@ -2,5 +2,13 @@ import { BirthdayGreetingService } from "./service";
 
 export async function main(args: string[]): Promise<void> {
 	const birthdayService = new BirthdayGreetingService("lovitolorenzo23@gmail.com", "smtp.gmail.com", 587);
-	await birthdayService.findEmployeesBirthdaysAndSendEmails("employeeData.txt");
+	const employeeWhoReceivedGreetings = await birthdayService.findEmployeesBirthdaysAndSendEmails("employeeData.txt");
+
+	console.log(
+		employeeWhoReceivedGreetings
+			? `Employees who have received the greetings email: ${employeeWhoReceivedGreetings
+					.map((employee) => `${employee.firstName} ${employee.lastName}`)
+					.join(", ")}`
+			: `No employees have received the greetings email!`,
+	);
 }
