735bb3c859df04ea9df849c8d45fcc3c6e706723
Lorenzo <lovitolorenzo@yahoo.it>
2023-07-24 18:00:18 +0200

    fix: changes names for the collection

--- birthday/schema/employees.ts
+++ birthday/schema/employees.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { Employee as EmployeeModel } from "../models/Employee";
 
-const UsersSchema = new mongoose.Schema({
+const EmployeeSchema = new mongoose.Schema({
 	lastName: {
 		type: String,
 		lowercase: true,
@@ -23,11 +23,11 @@ const UsersSchema = new mongoose.Schema({
 	},
 });
 
-UsersSchema.virtual("id").get(function () {
+EmployeeSchema.virtual("id").get(function () {
 	return this._id.toHexString();
 });
 
-UsersSchema.set("toJSON", {
+EmployeeSchema.set("toJSON", {
 	virtuals: true,
 	versionKey: false,
 	transform: function (doc, ret) {
@@ -35,6 +35,4 @@ UsersSchema.set("toJSON", {
 	},
 });
 
-const Users = mongoose.model<EmployeeModel>("users", UsersSchema, "users");
-
-export default Users;
+export const Employee = mongoose.model<EmployeeModel>("employees", EmployeeSchema, "employees");
