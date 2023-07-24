36125e2256bf7e9a3b89606d9f86cb3a8e477c82
Lorenzo <lovitolorenzo@yahoo.it>
2023-07-24 18:01:37 +0200

    feat: adds call for findInMongoDbEmployeesBirthdaysAndSendEmails method in main function; moves connectToMongoDBwithin main file

--- birthday/main.ts
+++ birthday/main.ts
@@ -1,8 +1,21 @@
+import mongoose from "mongoose";
 import { BirthdayGreetingService } from "./service";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 (async function main(): Promise<void> {
 	const birthdayService = new BirthdayGreetingService("lovitolorenzo23@gmail.com");
-	const employeeWhoReceivedGreetings = await birthdayService.findEmployeesBirthdaysAndSendEmails("employeeData.txt");
+
+	// const employeeWhoReceivedGreetings = await birthdayService.findInFsEmployeesBirthdaysAndSendEmails(
+	// 	"employeeData.txt",
+	// );
+
+	await connectToMongoDB();
+
+	const employeeWhoReceivedGreetings = await birthdayService.findInMongoDbEmployeesBirthdaysAndSendEmails();
+
+	mongoose.connection.close();
 
 	console.log(
 		employeeWhoReceivedGreetings.length > 0
@@ -12,3 +25,16 @@ import { BirthdayGreetingService } from "./service";
 			: `No employees have received the greetings email!`,
 	);
 })();
+
+export async function connectToMongoDB(): Promise<void> {
+	try {
+		if (!process.env.DATABASE_URI || !process.env.DATABASE_NAME) {
+			throw new Error("Missing DB env vars");
+		}
+		await mongoose.connect(process.env.DATABASE_URI, { dbName: process.env.DATABASE_NAME });
+		console.log("Connection with Mongodb successful");
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
+}
